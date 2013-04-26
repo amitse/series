2ce47ea437b5e8b077e55075d919891dd450a883
Amit Sengupta <amit.sengupta1@facebook.com>
2013-04-26 22:57:22 +0530

    commit 2

--- Question.java
+++ Question.java
@@ -2,51 +2,22 @@ import java.io.*;
 import java.util.Random;
 public class Question {
 	
-	/**
-	 * DIFFICULTY			MARKS
-	 * ===================================
-	 * EASY					1
-	 * MEDIUM				2-3
-	 * DIFFICULT			4-5
-	 * RANDOM				?
-	 * 
-	 * Easy
-	 * ===================================
-	 * question type
-	 * 1) AP							1
-	 * 2) GP							1
-	 * 3) sqauare series / cube series	1
-	 * 4) prime							1
-	 * 5) power series					1
-	 * 
-	 * MEDIUM
-	 * ===================================
-	 * 1) Easy							1
-	 * 2) Diffrence in GP				2
-	 * 3) n is in AP/GP					3
-	 * 4) n is PRIME					3
-	 * 5) fibonacchi					2
-	 * 6) introducing d (only AP or GP)	3
-	 * 7) AP + Power					2
-	 * 8) GP + Power					3
-	 * 
-	 * HARD
-	 * ===================================
-	 * 1) Medium						2
-	 * 2) Diffrence in medium			4
-	 * 2) n is in fibonachi				4
-	 * 3) AGP							4
-	 * 4) d is also in AP/GP (main is only in AP or in GP)
-	 * 									4
-	 * 5) AP + GP +POWER				5
-	 * 6) AP + GP + POWER + D			5
-	*/
-	
 	static final int EASY=1,MEDIUM=2,DIFFICULT=3,RANDOM=4;
-	
+	//(a+ n*b) * c ^k + d
 	static void generate (int difficulty)
 	{
-		//(a+ n*b) * c ^k + d
+		switch(difficulty)
+		{
+			case EASY:
+					break;
+			case MEDIUM:
+					break;
+			case DIFFICULT:
+					break;
+			case RANDOM:
+					break;
+		}
+		
 		Random r = new Random();
 		int a = r.nextInt(30);a-=15;
 		int b = r.nextInt(20);b-=10;
